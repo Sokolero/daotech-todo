121ec4d2afbbc454577cab90a068284ff09091e8
Roman Sokolenko <sokolero@yandex.ru>
2022-01-11 21:53:44 +0300

    update directories structure

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import "./App.scss";
+
+const App = (props) => {
+
+  return(
+    <div className="wrapper">
+      <div className="container">
+        <header className="header">Header</header>
+        <main className="main">Main</main>
+        <aside className="aside">Aside</aside>
+      </div>
+    </div>
+  );
+}
+
+export default App;
