a4cb9332113f923c41bd170f5fa1f2144b00127c
Roman Sokolenko <sokolero@yandex.ru>
2022-01-12 00:28:35 +0300

    pit-stop

--- src/App.js
+++ src/App.js
@@ -1,15 +1,42 @@
 import React from "react";
 
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import Button from "react-bootstrap/Button";
+
+
 import "./App.scss";
 
 const App = (props) => {
 
   return(
     <div className="wrapper">
-      <div className="container">
-        <header className="header">Header</header>
-        <main className="main">Main</main>
-        <aside className="aside">Aside</aside>
+      <header className="header d-flex justify-content-center align-items-center">
+        <h1>--- TODO ---</h1>
+      </header>
+      <div className="container grid-container">
+        <aside className="aside d-flex flex-column">
+          <button className="btn btn-primary d-flex align-items-center"><span className="plus">+</span> Create Task</button>
+        </aside>
+        <main className="main bg-primary rounded-3 text-secondary">
+          <div className="task-container rounded-3">
+            <div className="task-container__filters d-flex justify-content-center">
+              <button type="button" className="btn-lg btn-primary flex-grow-1 active">All</button>
+              <button type="button" className="btn-lg btn-primary flex-grow-1 ">Incomplete</button>
+              <button type="button" className="btn-lg btn-primary flex-grow-1 ">Done!</button>
+            </div>
+            <div className="task-container__body">
+              <ul className="task-list">
+                <li className="task-list__item">Task 1</li>
+                <li className="task-list__item">Task 2</li>
+                <li className="task-list__item">Task 2</li>
+              </ul>
+            </div>
+          </div>
+          <div className="note-container">
+            <h3 className="note__title">Комментарий к задаче Task 1</h3>
+            <p className="note__text">Срочно, звонить можно во вторник</p>
+          </div>
+        </main>
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,7 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+
+import "bootstrap/scss/bootstrap.scss";
 
 import App from "./App.js"
+
+// ---- Initial state --------------------------------------------------------
+const initialState = {
+  tasks: [], // { id, name, note, isDone }
+  activeFilter: "all", // all, incomplete, done
+  createTaskForm: "hidden", // show, hidden
+  activeTask: null,
+};
+
+// --- Reducer ----------------------------------------------------------------
+const reducer = (state=initialState, action) => {
+  switch (action.type) {
+    case "create_task":
+      return { ...state, tasks: [ ...state.tasks, action.payload.task ] }
+    case "done_task":
+      
+
+    default:
+      return state;
+
+  }
+}
 
 
 ReactDOM.render(
